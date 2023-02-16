0d169ff54482eb35214183c90cb031744e556868
Slugronaut <jcsluggy@gmail.com>
2023-02-16 03:12:03 -0500

    Implemented a basic Frustum culler.
    This will allow culling of 3D models based on the visibility of the billboard sprite. However it does not take into account any shadows cast by the billboard sprite so it can potentially cull 3D models too early.

--- Runtime/FrustumSpriteCuller.cs
+++ Runtime/FrustumSpriteCuller.cs
@@ -1,21 +1,92 @@
-using System.Collections;
+using Sirenix.OdinInspector;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace AdvLifeSim
+namespace ThreeDee
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class FrustumSpriteCuller : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [Tooltip("A list of GameObjects that will be set to active when this culler is visible in the camera and inactive otherwise.")]
+        public List<GameObject> ObjectsToCull;
+        public MeshRenderer SpriteBillboardRenderer;
+        public bool UseMainCamera = true;
+        [HideIf("UseMainCamera")]
+        public Camera Cam;
+
+
+        VisibilityStates LastVisibleState = VisibilityStates.Unset;
+
+        public enum VisibilityStates
         {
-        
+            Unset,
+            Visible,
+            Invisible,
         }
 
-        // Update is called once per frame
+        private static readonly Plane[] FrustrumPlanes = new Plane[6];
+
+
+        void Start()
+        {
+            if (UseMainCamera)
+                Cam = Camera.main;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         void Update()
         {
-        
+            //this will not scale well. we'll need a bool flag at some point
+            if (SpriteBillboardRenderer == null ||
+                Cam == null) return;
+
+            bool flag = IsVisible(Cam, SpriteBillboardRenderer.bounds);
+            foreach (var go in ObjectsToCull)
+            {
+                go.SetActive(flag);
+            }
+            /*
+            if(IsVisible(Cam, SpriteBillboardRenderer.bounds))
+            {
+                if (LastVisibleState == VisibilityStates.Visible) return;
+                else
+                {
+                    foreach (var go in ObjectsToCull)
+                    {
+                        if (!go.activeSelf) go.SetActive(true);
+                    }
+                }
+                LastVisibleState= VisibilityStates.Visible;
+            }
+            else
+            {
+                if (LastVisibleState == VisibilityStates.Invisible) return;
+                else
+                {
+                    foreach (var go in ObjectsToCull)
+                    {
+                        if (go.activeSelf) go.SetActive(false);
+                    }
+                }
+                LastVisibleState= VisibilityStates.Invisible;
+            }
+            */
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="bounds"></param>
+        /// <returns></returns>
+        private static bool IsVisible(Camera camera, Bounds bounds)
+        {
+            GeometryUtility.CalculateFrustumPlanes(camera, FrustrumPlanes);
+            return GeometryUtility.TestPlanesAABB(FrustrumPlanes, bounds);
         }
     }
 }
